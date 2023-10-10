ad861b07b94e05ecd5087dbfbe8b21112d9e44bb
Bale <189713619@qq.com>
2023-10-10 23:36:04 +0800

    feat: 更新UIGroup

--- Assets/UniEngine/Module/UIForm/UIGroup/UIGroup.cs
+++ Assets/UniEngine/Module/UIForm/UIGroup/UIGroup.cs
@@ -149,6 +149,95 @@ namespace UniEngine.Module.UIForm.UIGroup
                 _cachedNode = null;
             }
         }
+        
+        /// <summary>
+        /// 刷新界面组。
+        /// </summary>
+        public void Refresh()
+        {
+            var current = _uiFormInfos.First;
+            var pause = _pause;
+            var cover = false;
+            var depth = UIFormCount;
+            while (current != null && current.Value != null)
+            {
+                var next = current.Next;
+                current.Value.UIForm.OnDepthChanged(Depth, depth--);
+                if (current.Value == null)
+                {
+                    return;
+                }
+
+                if (pause)
+                {
+                    if (!current.Value.Covered)
+                    {
+                        current.Value.Covered = true;
+                        current.Value.UIForm.OnCover();
+                        if (current.Value == null)
+                        {
+                            return;
+                        }
+                    }
+
+                    if (!current.Value.Paused)
+                    {
+                        current.Value.Paused = true;
+                        current.Value.UIForm.OnPause();
+                        if (current.Value == null)
+                        {
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    if (current.Value.Paused)
+                    {
+                        current.Value.Paused = false;
+                        current.Value.UIForm.OnResume();
+                        if (current.Value == null)
+                        {
+                            return;
+                        }
+                    }
+
+                    if (current.Value.UIForm.PauseCoveredUIForm)
+                    {
+                        pause = true;
+                    }
+
+                    if (cover)
+                    {
+                        if (!current.Value.Covered)
+                        {
+                            current.Value.Covered = true;
+                            current.Value.UIForm.OnCover();
+                            if (current.Value == null)
+                            {
+                                return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (current.Value.Covered)
+                        {
+                            current.Value.Covered = false;
+                            current.Value.UIForm.OnReveal();
+                            if (current.Value == null)
+                            {
+                                return;
+                            }
+                        }
+
+                        cover = true;
+                    }
+                }
+
+                current = next;
+            }
+        }
 
         /// <summary>
         /// 界面组中是否存在界面。
@@ -380,95 +469,6 @@ namespace UniEngine.Module.UIForm.UIGroup
 
             _uiFormInfos.Remove(uiFormInfo);
             _uiFormInfos.AddFirst(uiFormInfo);
-        }
-
-        /// <summary>
-        /// 刷新界面组。
-        /// </summary>
-        public void Refresh()
-        {
-            var current = _uiFormInfos.First;
-            var pause = _pause;
-            var cover = false;
-            var depth = UIFormCount;
-            while (current != null && current.Value != null)
-            {
-                var next = current.Next;
-                current.Value.UIForm.OnDepthChanged(Depth, depth--);
-                if (current.Value == null)
-                {
-                    return;
-                }
-
-                if (pause)
-                {
-                    if (!current.Value.Covered)
-                    {
-                        current.Value.Covered = true;
-                        current.Value.UIForm.OnCover();
-                        if (current.Value == null)
-                        {
-                            return;
-                        }
-                    }
-
-                    if (!current.Value.Paused)
-                    {
-                        current.Value.Paused = true;
-                        current.Value.UIForm.OnPause();
-                        if (current.Value == null)
-                        {
-                            return;
-                        }
-                    }
-                }
-                else
-                {
-                    if (current.Value.Paused)
-                    {
-                        current.Value.Paused = false;
-                        current.Value.UIForm.OnResume();
-                        if (current.Value == null)
-                        {
-                            return;
-                        }
-                    }
-
-                    if (current.Value.UIForm.PauseCoveredUIForm)
-                    {
-                        pause = true;
-                    }
-
-                    if (cover)
-                    {
-                        if (!current.Value.Covered)
-                        {
-                            current.Value.Covered = true;
-                            current.Value.UIForm.OnCover();
-                            if (current.Value == null)
-                            {
-                                return;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (current.Value.Covered)
-                        {
-                            current.Value.Covered = false;
-                            current.Value.UIForm.OnReveal();
-                            if (current.Value == null)
-                            {
-                                return;
-                            }
-                        }
-
-                        cover = true;
-                    }
-                }
-
-                current = next;
-            }
         }
 
         internal void InternalGetUIForms(string uiFormAssetName, List<IUIForm> results)
